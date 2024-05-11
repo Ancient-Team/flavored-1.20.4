c6f9ec01fe34465173790ee92ceb620051ca72fe
K8bus <94112952+K8bus@users.noreply.github.com>
2024-05-11 10:02:25 +0200

    Translations fixed

--- src/main/java/ancient/flavored/datagen/EnglishLangProvider.java
+++ src/main/java/ancient/flavored/datagen/EnglishLangProvider.java
@@ -23,69 +23,69 @@ public class EnglishLangProvider extends FabricLanguageProvider {
 
         /* CAKES */
 
-        translationBuilder.add("blocks.flavored.berries_tart", "Berries Tart");
-        translationBuilder.add("blocks.flavored.chocolate_cake", "Chocolate Cake");
-        translationBuilder.add("blocks.flavored.cinnamon_cake", "Cinnamon Cake");
-        translationBuilder.add("blocks.flavored.honey_cake", "Honey Cake");
-        translationBuilder.add("blocks.flavored.mint_cake", "Mint Cake");
-        translationBuilder.add("blocks.flavored.vanilla_cake", "Vanilla Cake");
+        translationBuilder.add("block.flavored.berries_tart", "Berries Tart");
+        translationBuilder.add("block.flavored.chocolate_cake", "Chocolate Cake");
+        translationBuilder.add("block.flavored.cinnamon_cake", "Cinnamon Cake");
+        translationBuilder.add("block.flavored.honey_cake", "Honey Cake");
+        translationBuilder.add("block.flavored.mint_cake", "Mint Cake");
+        translationBuilder.add("block.flavored.vanilla_cake", "Vanilla Cake");
 
         /* ICE CREAM BLOCKS */
 
-        translationBuilder.add("blocks.flavored.berries_ice_cream_block", "Berries Ice Cream Block");
-        translationBuilder.add("blocks.flavored.chocolate_ice_cream_block", "Chocolate Ice Cream Block");
-        translationBuilder.add("blocks.flavored.honey_ice_cream_block", "Honey Ice Cream Block");
-        translationBuilder.add("blocks.flavored.mint_ice_cream_block", "Mint Ice Cream Block");
-        translationBuilder.add("blocks.flavored.vanilla_ice_cream_block", "Vanilla Ice Cream Block");
-        translationBuilder.add("blocks.flavored.watermelon_ice_cream_block", "Watermelon Ice Cream Block");
+        translationBuilder.add("block.flavored.berries_ice_cream_block", "Berries Ice Cream Block");
+        translationBuilder.add("block.flavored.chocolate_ice_cream_block", "Chocolate Ice Cream Block");
+        translationBuilder.add("block.flavored.honey_ice_cream_block", "Honey Ice Cream Block");
+        translationBuilder.add("block.flavored.mint_ice_cream_block", "Mint Ice Cream Block");
+        translationBuilder.add("block.flavored.vanilla_ice_cream_block", "Vanilla Ice Cream Block");
+        translationBuilder.add("block.flavored.watermelon_ice_cream_block", "Watermelon Ice Cream Block");
 
         /* CAKE SLICES */
 
-        translationBuilder.add("items.flavored.berries_tart_slice", "Berries Tart Slice");
-        translationBuilder.add("items.flavored.chocolate_cake_slice", "Chocolate Cake Slice");
-        translationBuilder.add("items.flavored.cinnamon_cake_slice", "Cinnamon Cake Slice");
-        translationBuilder.add("items.flavored.honey_cake_slice", "Honey Cake Slice");
-        translationBuilder.add("items.flavored.mint_cake_slice", "Mint Cake Slice");
-        translationBuilder.add("items.flavored.vanilla_cake_slice", "Vanilla Cake Slice");
+        translationBuilder.add("item.flavored.berries_tart_slice", "Berries Tart Slice");
+        translationBuilder.add("item.flavored.chocolate_cake_slice", "Chocolate Cake Slice");
+        translationBuilder.add("item.flavored.cinnamon_cake_slice", "Cinnamon Cake Slice");
+        translationBuilder.add("item.flavored.honey_cake_slice", "Honey Cake Slice");
+        translationBuilder.add("item.flavored.mint_cake_slice", "Mint Cake Slice");
+        translationBuilder.add("item.flavored.vanilla_cake_slice", "Vanilla Cake Slice");
 
         /* ICE CREAMS */
 
-        translationBuilder.add("items.flavored.berries_ice_cream", "Berries Ice Cream");
-        translationBuilder.add("items.flavored.chocolate_ice_cream", "Chocolate Ice Cream");
-        translationBuilder.add("items.flavored.honey_ice_cream", "Honey Ice Cream");
-        translationBuilder.add("items.flavored.mint_ice_cream", "Mint Ice Cream");
-        translationBuilder.add("items.flavored.vanilla_ice_cream", "Vanilla Ice Cream");
-        translationBuilder.add("items.flavored.watermelon_ice_cream", "Watermelon Ice Cream");
+        translationBuilder.add("item.flavored.berries_ice_cream", "Berries Ice Cream");
+        translationBuilder.add("item.flavored.chocolate_ice_cream", "Chocolate Ice Cream");
+        translationBuilder.add("item.flavored.honey_ice_cream", "Honey Ice Cream");
+        translationBuilder.add("item.flavored.mint_ice_cream", "Mint Ice Cream");
+        translationBuilder.add("item.flavored.vanilla_ice_cream", "Vanilla Ice Cream");
+        translationBuilder.add("item.flavored.watermelon_ice_cream", "Watermelon Ice Cream");
 
         /* SHAKES */
 
-        translationBuilder.add("items.flavored.berries_shake", "Berries Shake");
-        translationBuilder.add("items.flavored.chocolate_shake", "Chocolate Shake");
-        translationBuilder.add("items.flavored.honey_shake", "Honey Shake");
-        translationBuilder.add("items.flavored.mint_shake", "Mint Shake");
-        translationBuilder.add("items.flavored.vanilla_shake", "Vanilla Shake");
-        translationBuilder.add("items.flavored.watermelon_shake", "Watermelon Shake");
+        translationBuilder.add("item.flavored.berries_shake", "Berries Shake");
+        translationBuilder.add("item.flavored.chocolate_shake", "Chocolate Shake");
+        translationBuilder.add("item.flavored.honey_shake", "Honey Shake");
+        translationBuilder.add("item.flavored.mint_shake", "Mint Shake");
+        translationBuilder.add("item.flavored.vanilla_shake", "Vanilla Shake");
+        translationBuilder.add("item.flavored.watermelon_shake", "Watermelon Shake");
 
         /* SPECIAL ITEMS */
 
-        translationBuilder.add("items.flavored.mint_filled_chocolate", "Mint Filled Chocolate");
-        translationBuilder.add("items.flavored.vanilla_filled_chocolate", "Vanilla Filled Chocolate");
+        translationBuilder.add("item.flavored.mint_filled_chocolate", "Mint Filled Chocolate");
+        translationBuilder.add("item.flavored.vanilla_filled_chocolate", "Vanilla Filled Chocolate");
 
-        translationBuilder.add("items.flavored.mints", "Mints");
+        translationBuilder.add("item.flavored.mints", "Mints");
 
-        translationBuilder.add("items.flavored.marshmallow", "Marshmallow");
+        translationBuilder.add("item.flavored.marshmallow", "Marshmallow");
 
-        translationBuilder.add("items.flavored.hot_chocolate", "Hot Chocolate");
+        translationBuilder.add("item.flavored.hot_chocolate", "Hot Chocolate");
 
-        translationBuilder.add("items.flavored.cinnamon_rolls", "Cinnamon Rolls");
+        translationBuilder.add("item.flavored.cinnamon_rolls", "Cinnamon Rolls");
 
         /* UTILITY ITEMS */
 
-        translationBuilder.add("items.flavored.mug", "Mug");
+        translationBuilder.add("item.flavored.mug", "Mug");
 
         /* Other Shit */
 
-        translationBuilder.add("itemgroup.flavored", "Flavored");
+        translationBuilder.add("itemGroup.flavored", "Flavored");
 
 
 
